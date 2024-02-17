7ebf4df6f37ba2fe82a28807d60aca1a6af18c04
amandamachadodev <machado.amandag@gmail.com>
2024-02-17 20:23:30 -0300

    refactor: deleta arquivos não utilizados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
 import { PrismaService } from './prisma/prisma.service';
 import { TeamModule } from './team/team.module';
 
 @Module({
   imports: [PlayerModule, TeamModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
